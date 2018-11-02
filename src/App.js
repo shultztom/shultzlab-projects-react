0617e9dc7f936b1e13507453e167b1e3951898b3
shultztom <shultzthomask@sau.edu>
2018-11-02 13:44:45 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Card from "./Card";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        {
+          id: 1,
+          img: "/images/golfhandicapcalculator.PNG",
+          text: "Golf Handicap Calculator",
+          link: "https://golfhandicapcalculator.herokuapp.com/",
+          github: ""
+        },
+        {
+          id: 2,
+          img: "/images/xkcdviewer.png",
+          text: "XKCD Comic Viewer",
+          link: "https://shultztom.github.io/xkcd-viewer-react/",
+          github: "https://github.com/shultztom/xkcd-viewer-react"
+        },
+        {
+          id: 3,
+          img: "/images/xkcdapi.png",
+          text: "XKCD CORS API",
+          link: "https://xkcd-cors-api.herokuapp.com/",
+          github: "https://github.com/shultztom/xkcd-cors-api"
+        },
+        {
+          id: 4,
+          img: "/images/randomdoggeneratorreact.png",
+          text: "Random Dog Generator",
+          link: "https://shultztom.github.io/dog-generator-react/",
+          github: "https://github.com/shultztom/dog-generator-react"
+        },
+        {
+          id: 5,
+          img: "/images/dinofacts.png",
+          text: "Dinosaur Facts!",
+          link: "https://shultztom.github.io/dinosaur-fact-react/",
+          github: "https://github.com/shultztom/dinosaur-fact-react"
+        },
+        {
+          id: 6,
+          img: "/images/dinofactsapi.png",
+          text: "Dinosaur Facts! API",
+          link: "https://github.com/shultztom/dinosaur-fact-api",
+          github: "https://dinosaur-facts-api.herokuapp.com/"
+        }
+      ]
+    };
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div>
+        <header>
+          <div className="navbar navbar-dark bg-dark shadow-sm">
+            <div className="container d-flex justify-content-between">
+              <a href="/" className="navbar-brand d-flex align-items-center">
+                <strong>ShultzLab</strong>
+              </a>
+            </div>
+          </div>
         </header>
+
+        <main role="main">
+          <section className="jumbotron text-center">
+            <div className="container">
+              <h1 className="jumbotron-heading pt-1">ShultzLab Projects</h1>
+              <p className="lead text-muted">
+                Projects created by Tom Shultz. Some are hosted on my own
+                servers, while others are hosted on Heroku.
+              </p>
+              <p className="pb-1">
+                <a href="https://github.com/shultztom">
+                  <i className="fab fa-github-square fa-2x gh pr-2" />
+                </a>
+                <a href="https://www.linkedin.com/in/tomshultz/">
+                  <i className="fab fa-linkedin fa-2x li" />
+                </a>
+              </p>
+            </div>
+          </section>
+          <div className="album py-5 bg-light">
+            <div className="container">
+              <div className="row">
+                {this.state.data.map(data => (
+                  <Card key={data.id} data={data} />
+                ))}
+              </div>
+            </div>
+          </div>
+        </main>
+
+        <footer className="text-muted">
+          <div className="container">
+            <p>
+              This page is based off of the 'Album' example by Bootstrap. You
+              can view the example
+              <a href="https://getbootstrap.com/docs/4.1/examples/album/">
+                here
+              </a>
+            </p>
+          </div>
+        </footer>
       </div>
     );
   }
---
+++ src/Card.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+
+class Card extends Component {
+  render() {
+    const { data } = this.props;
+    return (
+      <div className="col-md-4">
+        <div className="card mb-4 shadow-sm">
+          <img className="card-img-top" src={data.img} alt="img" />
+          <div className="card-body">
+            <p className="card-text">{data.text}</p>
+            <div className="d-flex justify-content-between align-items-center">
+              <div className="btn-group">
+                <button
+                  type="button"
+                  className="btn btn-sm btn-outline-secondary"
+                  onClick={() => (window.location.href = data.link)}
+                >
+                  View
+                </button>
+                {data.github.length > 0 && (
+                  <button
+                    type="button"
+                    className="btn btn-sm btn-outline-secondary"
+                    onClick={() => (window.location.href = data.github)}
+                  >
+                    Source Code
+                  </button>
+                )}
+              </div>
+              {data.github.length === 0 && (
+                <span className="float-right">Private Repo</span>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Card;
